d0e800e196934e8053abc5d15a9a14a2836edc0b
Renato Alves Soares <renatoguara2020@gmail.com>
2022-09-22 13:06:27 -0300

    COMMIT CHANGES

---
+++ myFirstSite/Pessoa.py
@@ -0,0 +1,13 @@
+class Pessoa:
+    def __init__(self, nome, sexo, cpf):
+        self.nome = nome
+        self.sexo = sexo
+        self.cpf = cpf
+
+
+if __name__ == "__main__":
+    pessoa1 = Pessoa("RENATO ALVES SOARES", "Masculino", "123456")
+
+    print(pessoa1.nome)
+    print(pessoa1.cpf)
+    print(pessoa1.sexo)
